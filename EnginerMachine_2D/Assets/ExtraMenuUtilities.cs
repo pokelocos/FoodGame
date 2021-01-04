c0cbc7f7fb31017f8a872199f80d250b192db015
pokelocos <nare.rare.fire@gmail.com>
2021-01-04 13:06:11 -0300

    upload 1/?

---
+++ EnginerMachine_2D/Assets/ExtraMenuUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RA.Utilities
+{
+    public class ExtraMenuUtilities : MonoBehaviour
+    {
+        public void LoadScene(string name)
+        {
+            Debug.Log("Load Scene: " + name);
+            SceneManager.LoadScene(name);
+        }
+
+        public void LoadScene(int id)
+        {
+            Debug.Log("Load Scene: " + id);
+            SceneManager.LoadScene(id);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        //Open web page
+        public void OpenWebPage(string url)
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
